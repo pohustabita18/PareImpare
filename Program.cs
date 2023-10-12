2af2a454932c7c43ab1ea0b944ce85d3f83a0cf1
TabitaPohusUeo <147128333+pohustabita18@users.noreply.github.com>
2023-10-12 13:11:28 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AdunareA2numere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int s;
+            a = Convert.ToInt32(Console.ReadLine());
+            b= Convert.ToInt32(Console.ReadLine());
+            s = a + b;
+            Console.WriteLine(s);
+        }
+    }
+}
